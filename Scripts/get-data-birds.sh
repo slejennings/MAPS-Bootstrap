9edd64fb3a5ee5dc79655e8e005d480f8549b86c
Scott Appleby <156455989+s-appleby@users.noreply.github.com>
2025-03-26 16:38:36 -0700

    change wget to curl

--- Scripts/get-data-birds.sh
+++ Scripts/get-data-birds.sh
@@ -9,7 +9,9 @@ mkdir -p /data/MAPS/inputs
 cd /data/MAPS
 
 # Get full data file
-wget "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false" -O /inputs/adulttrends_boot.rds
+# wget "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false" -O /inputs/adulttrends_boot.rds
+curl -o /inputs/adulttrends_boot.rds "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false"
 
 # Get the data prep file (this should both divide the above data file into species-level files, and create a directory structure for the files to go into)
-wget "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R" -O bird_data_prep.R
+# wget "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R" -O bird_data_prep.R
+curl -o bird_data_prep.R "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R"
