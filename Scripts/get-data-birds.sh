5af35d14c95cbbe9dccd05ef0679bdb0f1cd7d08
Scott Appleby <156455989+s-appleby@users.noreply.github.com>
2025-03-26 16:20:14 -0700

    some updates to get script running

--- Scripts/get-data-birds.sh
+++ Scripts/get-data-birds.sh
@@ -9,7 +9,7 @@ mkdir -p /data/MAPS/inputs
 cd /data/MAPS
 
 # Get full data file
-wget "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false" -O adulttrends_boot.rds
+wget "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false" -O /inputs/adulttrends_boot.rds
 
 # Get the data prep file (this should both divide the above data file into species-level files, and create a directory structure for the files to go into)
 wget "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R" -O bird_data_prep.R
