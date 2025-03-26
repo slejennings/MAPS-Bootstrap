1cd23890e54084221ee7dd6fa50e5d25f504f26c
Scott Appleby <156455989+s-appleby@users.noreply.github.com>
2025-03-26 15:08:37 -0700

    script updates

--- Scripts/get-data-birds.sh
+++ Scripts/get-data-birds.sh
@@ -9,7 +9,7 @@ mkdir -p /data/MAPS/inputs
 cd /data/MAPS
 
 # Get full data file
-wget "https://drive.usercontent.google.com/downloa?id=1kuN0S9sZGzjQOtSCdUR_dfnhpLszIWlB&export=download&authuser=0&confirm=yes" -O adulttrends_boot.rds
+wget "https://drive.usercontent.google.com/download?id=1kuN0S9sZGzjQOtSCdUR_dfnhpLszIWlB&confirm=yes" -O adulttrends_boot.rds
 
 # Get the data prep file (this should both divide the above data file into species-level files, and create a directory structure for the files to go into)
 wget "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R" -O bird_data_prep.R
