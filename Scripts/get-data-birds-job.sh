d2fd69496fcddbd43744f08e646c76cf0380d7a2
Scott Appleby <156455989+s-appleby@users.noreply.github.com>
2025-03-26 19:47:35 -0700

    fixed typos

--- Scripts/get-data-birds-job.sh
+++ Scripts/get-data-birds-job.sh
@@ -1,17 +1,11 @@
 #!/bin/env bash
 
-mkdir -p /data/MAPS
-mkdir -p /data/MAPS/test/inputs
-mkdir -p /data/MAPS/test/outputs
-mkdir -p /data/MAPS/outputs
-mkdir -p /data/MAPS/inputs
-
 cd /data/MAPS
 
 # Get full data file
 # wget "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds?inline=false" -O /inputs/adulttrends_boot.rds
-curl -o inputs/adulttrends_boot.rds "https://gitlab.nrp-nautilus.io/sappleby/maps/-/raw/main/adulttrends_boot.rds"
+curl -o maps-run.sh "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/maps-run.sh"
 
 # Get the data prep file (this should both divide the above data file into species-level files, and create a directory structure for the files to go into)
 # wget "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R" -O bird_data_prep.R
-curl -o bird_data_prep.R "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/bird_data_prep.R"
+curl -o maps-run.R "https://raw.githubusercontent.com/slejennings/MAPS-Bootstrap/refs/heads/main/Scripts/maps-run.R"
